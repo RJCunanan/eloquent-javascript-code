19be709550d31a5754ba01a96bfa8d3c3a3fd2c0
RJ Cunanan <rjlugtuc@gmail.com>
2024-10-08 14:16:02 -0700

    Added the summary section of the chapter.

--- 9-Regular-Expressions/textbook-code.js
+++ 9-Regular-Expressions/textbook-code.js
@@ -632,4 +632,54 @@ console.log("58:", /🍎{3}/u.test("🍎🍎🍎"));    // → true
 
 // Summary:
 
-//
+// Regexp are objects that represent patterns in strings. They use their
+// own language to express these patterns.
+
+/*
+
+/abc/       -> A sequence of characters
+/[abc]/     -> Any character from a set of characters
+/[^abc]/    -> Any character NOT in a set of characters
+/[0-9]/     -> Any character in a range of characters
+/x+/        -> One or more occurrences of the pattern x
+/x+?/       -> One or more occurrences, nongreedy
+/x* /       -> Zero or more occurrences
+/x?/        -> Zero or one occurrence
+/x{2,4}/    -> Two to four occurrences
+/(abc)/     -> A group
+/a|b|c/     -> Any one of several patterns
+/\d/        -> Any digit character
+/\w/        -> An alphanumeric character ("word character")
+/\s/        -> Any whitespace character
+/./         -> Any character except newlines
+/\p{L}/u    -> Any letter character
+/^/         -> Start of input
+/$/         -> End of input
+/(?=a)/     -> A look-ahead test   
+
+*/
+
+// A regex has a method test() to test whether a given string matches
+// it. It also has method exec() that, when a match is found, returns
+// an array containing all matched groups. Such an array has an "index"
+// property that indicates where the match started.
+
+// Strings have a match() method to match them agains a regex and a
+// search() method to search for one, returning only the starting
+// position of the match. Their replace() method can replace matches
+// of a pattern with a replacement string or function.
+
+// Regex can have options, written after the closing slash. The "i" option
+// makes the match case insensitive. The "g" option makes the expression
+// global, which, among other things, causes the replace() method to
+// replace all instances instead of just the first. The "y" option makes an
+// expression "sticky", which means that it will not search ahead and skip
+// part of the string when looking for a match. The "u" option turns on
+// Unicode mode, which enables \p syntax and fixes a number of problems around
+// the handling of characters that take up two code units.
+
+// Regex are a shart tool with an awkward handle. They simplify some tasks
+// tremendously but can quickly become unmanageable when applied to complex
+// problems. Part of knowing how to use them is resisting the urge to try
+// to shoehorn things into them that they cannot cleanly express.
+
