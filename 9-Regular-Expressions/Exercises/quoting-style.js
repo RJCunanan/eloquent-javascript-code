3beaba93353cbd421798a333c36895beb6be20fe
RJ Cunanan <rjlugtuc@gmail.com>
2024-10-14 16:52:26 -0700

    Added my solution to regexp quoting style exercise problem.

--- 9-Regular-Expressions/Exercises/quoting-style.js
+++ 9-Regular-Expressions/Exercises/quoting-style.js
@@ -6,6 +6,13 @@ Think of a pattern that distinguishes these two kinds of quote usage and craft a
 
 
 let text = "'I'm the cook,' he said, 'it's my job.'";
-// Change this call.
-console.log(text.replace(/A/g, "B"));
+
+/*
+Single quotes (') that need to be replaced:
+- the ' at the very beginning of the string
+- a non-word character followed by a '
+- a ' followed by a non-word character
+- the ' at the very end of the string
+*/
+console.log(text.replace(/^'|(\P{L})'|('\P{L})|'$/gu, "$1\"$2"));
 // → "I'm the cook," he said, "it's my job."
